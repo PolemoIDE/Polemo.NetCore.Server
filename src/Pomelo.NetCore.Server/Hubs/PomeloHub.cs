1089428d94189da39b6ed72ac79d74ae4be35b6f
Afa Cheng <hmasterwang@hotmail.com>
2016-05-18 17:07:11 +0800

    Added Azure package; OnConnected skeleton

--- src/Pomelo.NetCore.Server/Hubs/PomeloHub.cs
+++ src/Pomelo.NetCore.Server/Hubs/PomeloHub.cs
@@ -6,14 +6,37 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.NetCore.Server.Models;
+using Pomelo.NetCore.Azure;
 
 namespace Pomelo.NetCore.Server.Hubs
 {
     public partial class PomeloHub : Hub
     {
-        public override Task OnConnected()
+        public override async Task OnConnected()
         {
-            return base.OnConnected();
+            var DB = Context.Request.HttpContext.RequestServices.GetRequiredService<PomeloContext>();
+            //var SignInManager = Context.Request.HttpContext.RequestServices.GetRequiredService<SignInManager<User>>();
+            var UserManager = Context.Request.HttpContext.RequestServices.GetRequiredService<UserManager<User>>();
+            var User = await UserManager.FindByNameAsync(Context.User.Identity.Name);
+            var Claims = await UserManager.GetClaimsAsync(User);
+
+            var NodeClaim = Claims.Where(x => x.Type == "Owned VM");
+            if (NodeClaim.Count() == 0)
+            {
+                VMManagement VMMgr = new VMManagement("subscriptionId", "tenantId", "clientId", "appPassword");
+                //VMMgr.CreateVirtualMachineAsync()
+            }
+            else
+            {
+                var NodeId = Guid.Parse(NodeClaim.First().Value);
+                var Node = DB.Nodes.Where(x => x.Id == NodeId).First();
+                var IP = Node.IP;
+
+                // Start VM
+            }
+
+
+            await base.OnConnected();
         }
 
         public override Task OnDisconnected(bool stopCalled)
